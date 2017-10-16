04085b07e07f965cc96ad579b46b7ad2f1ca6939
kadinvanvalin <kadinvanvalin@yahoo.com>
2017-10-16 12:46:33 -0500

    add guard

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 console.log("hey");
 
 function GameTwentyFourtyEight() {
-  this.grid = [new Cell(4), new Cell(0),new Cell(2),new Cell(2)]
+  this.grid = [new Cell(4), new Cell(2),new Cell(2),new Cell(0)]
 }
 
 function Cell (givenValue) {
@@ -18,19 +18,22 @@ GameTwentyFourtyEight.prototype.moveRight = function() {
   for(var i = this.grid.length - 2; i >= 0; i-- ){
 
     var zeroCheck = i
-    while(zeroCheck + 1 < this.grid.length - 1 && this.grid[zeroCheck + 1].value === 0){
+    // check zero move right
+    while(this.grid[zeroCheck + 1] !== undefined && this.grid[zeroCheck + 1].value === 0){
       this.grid[zeroCheck+1].value = this.grid[zeroCheck].value
       console.log("while loop")
       this.grid[zeroCheck].value = 0
       zeroCheck++
     }
-    if (this.grid[i].value === 0) {
+    // dont mark zero as merged
+    if (this.grid[zeroCheck].value === 0) {
       console.log('fuckin a');
-      console.log(this.grid[i].merged);
-    } else if(this.grid[i].value === this.grid[i+1].value) {
-      this.grid[i].value = 0;
-      this.grid[i+1].value = this.grid[i+1].value * 2;
-      this.grid[i+1].merged = true;
+      console.log(this.grid[zeroCheck].merged);
+      // merge if you are a match
+    } else if(this.grid[zeroCheck + 1] !== undefined && this.grid[zeroCheck].value === this.grid[zeroCheck+1].value) {
+      this.grid[zeroCheck].value = 0;
+      this.grid[zeroCheck+1].value = this.grid[zeroCheck+1].value * 2;
+      this.grid[zeroCheck+1].merged = true;
     }
 
   }
