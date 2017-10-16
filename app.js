71cb61eba33132f43965f1a60f29367d95c17363
kadinvanvalin <kadinvanvalin@yahoo.com>
2017-10-16 12:31:43 -0500

    add move right function

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
 console.log("hey");
+
+function GameTwentyFourtyEight() {
+  this.grid = [new Cell(4), new Cell(0),new Cell(2),new Cell(2)]
+}
+
+function Cell (givenValue) {
+  this.merged = false;
+  this.value = givenValue;
+}
+
+GameTwentyFourtyEight.prototype.setup = function(first_argument) {
+  // body...
+  // makeSomeCells()
+};
+
+GameTwentyFourtyEight.prototype.moveRight = function() {
+  for(var i = this.grid.length - 2; i >= 0; i-- ){
+
+    var zeroCheck = i
+    while(zeroCheck + 1 < this.grid.length - 1 && this.grid[zeroCheck + 1].value === 0){
+      this.grid[zeroCheck+1].value = this.grid[zeroCheck].value
+      console.log("while loop")
+      this.grid[zeroCheck].value = 0
+      zeroCheck++
+    }
+    if (this.grid[i].value === 0) {
+      console.log('fuckin a');
+      console.log(this.grid[i].merged);
+    } else if(this.grid[i].value === this.grid[i+1].value) {
+      this.grid[i].value = 0;
+      this.grid[i+1].value = this.grid[i+1].value * 2;
+      this.grid[i+1].merged = true;
+    }
+
+  }
+
+}
+var mygame = new GameTwentyFourtyEight
+
+
+mygame.moveRight();
